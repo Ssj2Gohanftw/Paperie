713351d17418590aa04244119b748d5cffe20218
Ethan D'Costa <ethanmdcosta@gmail.com>
2025-04-06 23:31:29 +0530

    refactor: update import quotes, clean up contact page content, and replace anchor with Link component

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,27 +1,24 @@
-import ContactForm from '@/components/ui/contact/ContactForm';
+import ContactForm from "@/components/ui/contact/ContactForm";
+import Link from "next/link";
 
 export default function Contact() {
   return (
     <div className="min-h-screen bg-background px-4 py-16 flex flex-col items-center">
-      <div className="text-center text-[#A79C92] max-w-2xl mb-10">
+      <div className="text-center max-w-2xl mb-10">
         <h1 className="text-5xl uppercase tracking-wide mb-4 font-[var(--font-cormorant-garamond)]">
           Contact
         </h1>
         <p className="text-lg">
-          If you’re interested in working with us, the best way to reach us is by filling out the form below.
-          If this form doesn’t suit your inquiry, please reach out to{' '}
-          <a
-            href="mailto:info@paperie.com"
-            className="underline"
-          >
+          If you&apos;re interested in working with us, the best way to reach us
+          is by filling out the form below. If this form doesn&apos;t suit your
+          inquiry, please reach out to <br />
+          <Link href="mailto:info@paperie.com" className="underline">
             info@paperie.com
-          </a>
+          </Link>
         </p>
       </div>
 
-      {/* Contact form component */}
       <ContactForm />
-
     </div>
   );
 }
